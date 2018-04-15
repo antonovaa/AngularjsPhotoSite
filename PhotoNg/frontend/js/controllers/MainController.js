17d08fb9b992d45982934de896746d13665de807
antonovaa <antonovaa2904@gmail.com>
2018-04-15 20:46:14 +0300

    a little bit more fix

--- PhotoNg/frontend/js/controllers/MainController.js
+++ PhotoNg/frontend/js/controllers/MainController.js
@@ -12,37 +12,10 @@
 //         });
 //     };
 // });
-ModuleApp2.directive('onScrollToBottom', function ($document) {
-    //This function will fire an event when the container/document is scrolled to the bottom of the page
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-
-            var doc = angular.element($document)[0].body;
-
-            $document.bind("scroll", function () {
-
-                //console.log('in scroll');
-                //console.log("scrollTop + offsetHeight:" + (doc.scrollTop + doc.offsetHeight));
-                //console.log("scrollHeight: " + doc.scrollHeight);
-
-                if (doc.scrollTop + doc.offsetHeight >= doc.scrollHeight) {
-                    //run the event that was passed through
-                    scope.$apply(attrs.onScrollToBottom);
-                    console.log("test");
-
-                }
-            });
-        }
-    };
-});
 ModuleApp2.controller('MainController', function ($scope,$rootScope,$http) {
 
-    // $scope.test = function (a) {
-    //     console.log(a);
-    // };
-
 // ----------- todo work it
+    //Listeners
     $rootScope.$on('myEvent', function(event, args){
        console.log(args);
     });
@@ -50,5 +23,4 @@ ModuleApp2.controller('MainController', function ($scope,$rootScope,$http) {
     $scope.logFunc=function ($rootScope) {
         console.log("testtesttest"+$rootScope);
     }
-
 });
